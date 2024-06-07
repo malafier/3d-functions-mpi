2c67ed41d27df5b26bb16f680363cdbcfe488036
Robert Małafiejew <01169593@pw.edu.pl>
2024-06-07 17:44:02 +0200

    sequential trapezoid completed

--- calculator.py
+++ calculator.py
@@ -16,20 +16,37 @@ def schwefel_fun(x, y):
 
 class CalculationStrategy(ABC):
     @abstractmethod
-    def calculate(self, fun, x_start, x_end, y_start, y_end):
+    def calculate(self, fun, x_start, x_end, y_start, y_end, n):
         pass
 
 
-class SequentialStrategy(CalculationStrategy):
-    def calculate(self, fun, x_start, x_end, y_start, y_end):
+class SequentialTrapezoidStrategy(CalculationStrategy):
+    def calculate(self, fun, x_start, x_end, y_start, y_end, n):
+        h_x = (x_end - x_start) / n
+        h_y = (y_end - y_start) / n
+        result = 0
+        for i in range(n):
+            for j in range(n):
+                x_1 = x_start + i * h_x
+                x_2 = x_start + (i + 1) * h_x
+                y_1 = y_start + j * h_y
+                y_2 = y_start + (j + 1) * h_y
+
+                z_1, z_2, z_3, z_4 = fun(x_1, y_1), fun(x_2, y_1), fun(x_1, y_2), fun(x_2, y_2)
+                result += (z_1 + z_2 + z_3 + z_4) / 4 * h_x * h_y
+        return result
+
+
+class TrapezoidStrategy(CalculationStrategy):
+    def calculate(self, fun, x_start, x_end, y_start, y_end, n):
+        pass
+
+
+class SequentialMonteCarloStrategy(CalculationStrategy):
+    def calculate(self, fun, x_start, x_end, y_start, y_end, n):
         pass
 
 
 class MonteCarloStrategy(CalculationStrategy):
-    def calculate(self, fun, x_start, x_end, y_start, y_end):
-        pass
-
-
-class TrapezoidStrategy(CalculationStrategy):
-    def calculate(self, fun, x_start, x_end, y_start, y_end):
+    def calculate(self, fun, x_start, x_end, y_start, y_end, n):
         pass
